f214b660a2ecd0464b1df59e29b8ee42a8525e38
Saerom Hong <romy@romyromy.com>
2014-11-27 20:24:10 -0800

    Syncs before upload

--- logic.js
+++ logic.js
@@ -3,3 +3,23 @@ console.log('why');
 
 var one = document.getElementById("_1");
 console.log(one);
+
+$("#_1").click(function() {
+  $("#results").text( "bicky" )
+});
+
+$("#_2").click(function() {
+  $("#results").text( "2" )
+});
+
+$("#_3").click(function() {
+  $("#results").text( "3" )
+});
+
+/*create a function which takes a CSS ID, like `$('#_1')`
+ * and gets that id's TEXT content, and appends the 
+ * text to an empty array (or an existing array), and puts that
+ * in the `#results` css element's text.
+*/
+
+
