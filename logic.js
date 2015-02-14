c4846b7fa65fefc7a308ff911ab38c252f8a3f09
Saerom Hong <romy@romyromy.com>
2015-02-14 15:00:44 -0800

    Updates for Bicky

--- logic.js
+++ logic.js
@@ -16,6 +16,16 @@ $("#_3").click(function() {
   $("#results").text( "3" )
 });
 
+console.log("PAY ATTENTION:");
+$(".integer").each(function(key,value) {
+  console.log("key is:" + key + ", value is:" + value);
+  value.on( "click", function() {
+    console.log(value.textContent);
+  });
+});
+console.log("DONE");
+console.log($(".integer"));
+
 /*create a function which takes a CSS ID, like `$('#_1')`
  * and gets that id's TEXT content, and appends the 
  * text to an empty array (or an existing array), and puts that
